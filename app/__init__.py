b12ef672ed20b5c888b44ffa53c1ca1b90359a53
Jasonzhu12397 <jason.a.zhu@gmail.com>
2025-12-30 13:45:27 +0800

    Update __init__.py

--- app/__init__.py
+++ app/__init__.py
@@ -1,1 +1,10 @@
-~�&~V��)���E��$
+from flask import Flask
+
+def create_app() -> Flask:
+    app = Flask(__name__)
+    app.config.from_object("app.core.config.Config")
+
+    from app.api.endpoints import rag_bp
+    app.register_blueprint(rag_bp, url_prefix="/api/rag")
+
+    return app
