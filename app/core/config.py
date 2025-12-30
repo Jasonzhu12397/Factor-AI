aba0a2d21d81fe4a7ca99d20a4ce937e5a1c9e3c
Jasonzhu12397 <jason.a.zhu@gmail.com>
2025-12-30 13:42:54 +0800

    Update config.py

--- app/core/config.py
+++ app/core/config.py
@@ -1,1 +1,14 @@
-�jh��,~�&�'
+import os
+from pydantic import BaseSettings
+
+class Config(BaseSettings):
+    DEBUG: bool = True
+    PROJECT_NAME: str = "RAG-Backend"
+    VECTOR_DB_TYPE: str = "chroma"
+    VECTOR_DB_PERSIST_DIR: str = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../../data"))
+    EMBEDDING_DIM: int = 768
+    EMBEDDING_MODEL: str = "all-MiniLM-L6-v2"
+    MODEL_DEVICE: str = "cpu"
+    SUPPORTED_FILE_TYPES: list = [".txt", ".pdf", ".docx"]
+
+config = Config()
